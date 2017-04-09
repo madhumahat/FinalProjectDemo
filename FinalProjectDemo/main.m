7f9ef6eef655a8f0e79bd744f6beddc3412d9d8c
Madhu Mahat <madhumahat@Madhus-MacBook-Pro.local>
2017-04-08 21:15:06 -0400

    Initial Commit

---
+++ FinalProjectDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinalProjectDemo
+//
+//  Created by Madhu Mahat on 4/8/17.
+//  Copyright © 2017 Madhu Mahat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
